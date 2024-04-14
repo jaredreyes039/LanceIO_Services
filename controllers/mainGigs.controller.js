8841faf8210b044e862df5989038d708dc704cee
Jay <jaredreyes039@gmail.com>
2024-04-14 12:14:20 -0700

    Trying to fix cors deployment error

--- controllers/mainGigs.controller.js
+++ controllers/mainGigs.controller.js
@@ -1,17 +1,17 @@
 const { tokenVerificationWrapper } = require("../middleware/auth.middleware");
 const GigBasic = require('../models/gig.model');
 
-exports.getGigsByUserId = async (req, res) =>{
-    const {user_id, token} = req.params;
+exports.getGigsByUserId = async (req, res) => {
+    const { user_id, token } = req.params;
     console.log(req.params)
-    tokenVerificationWrapper(req, res, async ()=>{
-        try{
-            const gigs = await GigBasic.find({user_id: user_id})
+    tokenVerificationWrapper(req, res, async () => {
+        try {
+            const gigs = await GigBasic.find({ user_id: user_id })
             res.status(200).json(gigs)
         }
-        catch(err){
+        catch (err) {
             console.log(err)
-            return res.status(500).json({message: "Internal Server Error"})
+            res.status(500).json({ message: "Internal Server Error" })
         }
     }, token)
 }
